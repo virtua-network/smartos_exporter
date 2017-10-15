e392282040eac4b819fd464359d67ece263df734
mguegan <mguegan@virtua.ch>
2017-10-15 17:57:30 +0200

    Fix label in iostat collector

--- collector/iostat.go
+++ collector/iostat.go
@@ -66,9 +66,9 @@ func (e *gzDiskErrorsExporter) parseIostatOutput(out string) (error) {
         if err != nil {
             return err
         }
-        e.gzDiskErrors.With(prometheus.Labels{"device":deviceName,"type":"soft"}).Add(softErr)
-        e.gzDiskErrors.With(prometheus.Labels{"device":deviceName,"type":"hard"}).Add(hardErr)
-        e.gzDiskErrors.With(prometheus.Labels{"device":deviceName,"type":"trn"}).Add(trnErr)
+        e.gzDiskErrors.With(prometheus.Labels{"device":deviceName,"error_type":"soft"}).Add(softErr)
+        e.gzDiskErrors.With(prometheus.Labels{"device":deviceName,"error_type":"hard"}).Add(hardErr)
+        e.gzDiskErrors.With(prometheus.Labels{"device":deviceName,"error_type":"trn"}).Add(trnErr)
     }
     return nil
 }
