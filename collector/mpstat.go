3cd16a66878cb5d709771878cf21c962f39dc0d1
mguegan <mguegan@virtua.ch>
2017-10-15 06:58:26 +0200

    Remove comments and tag mpstat collector as broken

--- collector/mpstat.go
+++ collector/mpstat.go
@@ -4,6 +4,9 @@
 //  - gather CPU metrics
 //  - feed the collector
 
+// XXX COLLECTOR BROKEN
+// $(mpstat 1 1) always returns the same value
+
 package collector
 
 import (
@@ -19,12 +22,6 @@ type gzCpuUsageExporter struct {
     gzCpuUsage      *prometheus.GaugeVec
 }
 
-//type LoadAverageExporter struct {
-//    LoadAverage1    prometheus.Gauge
-//    LoadAverage5    prometheus.Gauge
-//    LoadAverage15   prometheus.Gauge
-//}
-
 func NewGzCpuUsageExporter() (*gzCpuUsageExporter, error) {
     return &gzCpuUsageExporter{
         gzCpuUsage: prometheus.NewGaugeVec(prometheus.GaugeOpts{
@@ -34,43 +31,14 @@ func NewGzCpuUsageExporter() (*gzCpuUsageExporter, error) {
     }, nil
 }
 
-//func NewLoadAverageExporter() (*LoadAverageExporter, error) {
-//    return &LoadAverageExporter{
-//        LoadAverage1: prometheus.NewGauge(prometheus.GaugeOpts{
-//            Name: "smartos_cpu_load1",
-//            Help: "CPU load average 1 minute.",
-//        }),
-//        LoadAverage5: prometheus.NewGauge(prometheus.GaugeOpts{
-//            Name: "smartos_cpu_load5",
-//            Help: "CPU load average 5 minutes.",
-//        }),
-//        LoadAverage15: prometheus.NewGauge(prometheus.GaugeOpts{
-//            Name: "smartos_cpu_load15",
-//            Help: "CPU load average 15 minutes.",
-//        }),
-//    }, nil
-//}
-
 func (e *gzCpuUsageExporter) Describe(ch chan<- *prometheus.Desc) {
     e.gzCpuUsage.Describe(ch)
 }
-
-//func (e *LoadAverageExporter) Describe(ch chan<- *prometheus.Desc) {
-//    ch <- e.LoadAverage1.Desc()
-//    ch <- e.LoadAverage5.Desc()
-//    ch <- e.LoadAverage15.Desc()
-//}
 
 func (e *gzCpuUsageExporter) Collect(ch chan<- prometheus.Metric) {
     e.mpstat()
     e.gzCpuUsage.Collect(ch)
 }
-//func (e *LoadAverageExporter) Collect(ch chan<- prometheus.Metric) {
-//    e.uptime()
-//    ch <- e.LoadAverage1
-//    ch <- e.LoadAverage5
-//    ch <- e.LoadAverage15
-//}
 
 func (e *gzCpuUsageExporter) mpstat() {
     out, eerr := exec.Command("mpstat", "1", "1").Output()
@@ -82,17 +50,6 @@ func (e *gzCpuUsageExporter) mpstat() {
         log.Fatal(perr)
     }
 }
-
-//func (e *LoadAverageExporter) uptime() {
-//    out, eerr := exec.Command("uptime").Output()
-//    if eerr != nil {
-//        log.Fatal(eerr)
-//    }
-//    perr := e.parseUptimeOutput(string(out))
-//    if perr != nil {
-//        log.Fatal(perr)
-//    }
-//}
 
 func (e *gzCpuUsageExporter) parseMpstatOutput(out string) (error) {
     outlines := strings.Split(out, "\n")
@@ -119,28 +76,3 @@ func (e *gzCpuUsageExporter) parseMpstatOutput(out string) (error) {
     }
     return nil
 }
-
-//func (e *LoadAverageExporter) parseUptimeOutput(out string) (error) {
-//    // we will use regex in order to be sure to catch good numbers
-//    r,_ := regexp.Compile(`load average: (\d.\d+), (\d.\d+), (\d.\d+)`)
-//    loads := r.FindStringSubmatch(out)
-//
-//    load1, err := strconv.ParseFloat(loads[1], 64)
-//    if err != nil {
-//        return err
-//    }
-//    load5, err := strconv.ParseFloat(loads[2], 64)
-//    if err != nil {
-//        return err
-//    }
-//    load15, err := strconv.ParseFloat(loads[3], 64)
-//    if err != nil {
-//        return err
-//    }
-//
-//    e.LoadAverage1.Set(load1)
-//    e.LoadAverage5.Set(load5)
-//    e.LoadAverage15.Set(load15)
-//
-//    return nil
-//}
